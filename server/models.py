aaa7b80408de6adb740eacbc9352ef0e072fbc81
Mutindi Hope <msndindi@outlook.com>
2023-06-25 17:03:11 +0300

    Completed Assignment

--- server/models.py
+++ server/models.py
@@ -5,6 +5,9 @@ metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
+
+
+
 db = SQLAlchemy(metadata=metadata)
 
 class Owner(db.Model):
